a63d8c8f6a69385df8214453b1f3af526be49eac
Joe Pattenden <151946049+JojojoMohojo@users.noreply.github.com>
2024-05-08 22:19:26 +0100

    add working display

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+let nine = document.querySelector("#nine");
+let zero = document.querySelector("#zero");
+let divi = document.querySelector("#divi");
+let mult = document.querySelector("#mult");
+let sub = document.querySelector("#sub");
+let plus = document.querySelector("#plus");
+let equals = document.querySelector("#equals");
+let dot = document.querySelector("#dot");
+let clear = document.querySelector("#clear");
+let display = document.querySelector(".display");
+
 function add(a, b) {
     return a + b;
 }
@@ -34,3 +53,16 @@ function operate(num1, num2, op) {
             break;
     }
 }
+let displayValue;
+document.addEventListener("DOMContentLoaded", () => {
+    let buttons = document.querySelectorAll("button");
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (event.target.id !== "dot" && event.target.id !== "equals" && event.target.id !== "clear") {
+                display.textContent = display.textContent + event.target.textContent;
+                displayValue = display.textContent;
+            }
+        });
+    });
+});
